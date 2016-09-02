9ae78aab14f9949348b1f5a7aa6363b33f6833b3
wujunkui <wujunkui407@icloud.com>
2016-09-02 22:37:33 +0800

    优化

---
+++ src/web_apps/statics/myjs/bills.js
@@ -0,0 +1,50 @@
+
+$('#date_select').datepicker();
+    //console.log($('#date_select').attr('name'));
+
+//$('#MyTable').datatable({
+//    pageSize: 15,
+//    sort: '*',
+//    option:{"onclick":"test();"}
+//});
+// $("#MyTable td").attr("onclick","test();")
+// $("#MyTable tr td:nth-child(2)").attr("onclick","test();")
+
+function test(){
+    $('#test_dialog').dialog();
+    }
+
+function eject(id){
+    $(id).modal();
+}
+
+
+function test2(){
+    $('#myModal').modal();
+}
+
+function insert_data(){
+    var new_date = $('#date_select').val();
+    var action = $('#action').val();
+    var cast = $('#cast').val();
+    $.ajax({
+    type:"post",
+    url:'/bills/upload',
+    data:{
+        "new_date":new_date,
+        "new_action":action,
+        "new_cast":cast,
+    },
+    success:function(res){
+        location.reload();
+    }
+    });
+}
+
+function update_data(id){
+    var input_val = $(id).val();
+    console.log(input_val);
+}
+
+
+
